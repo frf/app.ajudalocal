f47395e0d5742268128f9684a80f5ef7a0198146
Fabio Rocha <fabio@fabiofarias.com.br>
2021-02-22 08:55:53 -0300

    maps

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet'
 import { Link } from "react-router-dom";
@@ -8,26 +8,60 @@ import mapMarkerImg from "../../assets/images/iconeLogo.png";
 import Map from '../../components/Map';
 
 import './styles.css';
+import api from "../../services/api";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
-
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2]
 })
 
+export interface IPoint {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string; 
+}
+
+
 export default function OrphanagesMap() {
+    const [locales, setLocale] = useState<IPoint[]>([]);
+    const [position, setPosition] = useState({
+      latitude: 0,
+      longitude: 0,
+    });
+    useEffect(() => {
+      async function getDocs() {
+        const response = await api.get('/locales');
+        // response.catch(error => {
+        //   enqueueSnackbar('Falha conectar no servidor', { 
+        //       variant: 'error',
+        //   });
+        // });
+        setLocale(response.data.data);
+       
+        const pos: any = {
+          latitude: response.data.data[0].latitude,
+          longitude: response.data.data[0].longitude,
+        }
+        setPosition(pos);
+        console.log(pos);
+      }
+
+      getDocs()
+    }, [locales]);
+    
   return (
     <div id="page-map">
       <aside>
         <header>
           <img src={mapMarkerImg} alt="AjudaLocal" />
 
-          <h2>1 - Escolher um local para ajudar</h2>
+          <h2>1 - Escolha um local para ajudar</h2>
           <p>Veja a descrição deste local e vá até ele para poder ajudar.</p>
 
-          <h2>2 - Marque um local que precise de ajuda</h2>
+          <h2>2 - Marque um local que precisa de ajuda</h2>
           <p>Se você acha que neste local tem pessoas ou animais que possam precisar dde ajudar marque este local.</p>
         </header>
 
@@ -35,15 +69,17 @@ export default function OrphanagesMap() {
         </footer>
       </aside>
 
-      <Map>
-        <Marker icon={happyMapIcon} position={[-27.2092052,-49.6401092]}>
+      <Map center={[position.latitude, position.longitude]} zoom={14}>
+        {locales.map(locale => (
+        <Marker key={locale.id} icon={happyMapIcon} position={[locale.latitude,locale.longitude]}>
           <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-            Lar das meninas
+            {locale.name}
             <Link to={`/orphanages/1`}>
               <FiArrowRight size={20} color="#fff" />
             </Link>
           </Popup>
         </Marker>
+        ))}
       </Map>
 
       <Link to="/orphanages/create" className="create-orphanage">
---
+++ src/services/api.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.ajudalocal.me/v1",
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const url = await config?.url?.endsWith("login");
+    const tokenUser = sessionStorage.getItem("@App:access_token");
+
+    if (!url && tokenUser) {
+      config.headers.Authorization = `Bearer ${tokenUser}`;
+      config.headers.Accept = "application/json";
+      config.headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+      };
+    }
+
+    return config;
+  },
+  (error) => {
+    // I cand handle a request with errors here
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // You can even test for a response code
+    // and try a new request before rejecting the promise
+    if (error.response.status === 401) {
+      const requestConfig = error.config;
+      return axios(requestConfig);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
